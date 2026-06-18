55fb0068eba9dc13b89b153c884aed026ab3b6bf
durgesh033 <durgeshrout8@gmail.com>
2026-06-19 01:53:05 +0530

    Landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Tilt from "react-parallax-tilt";
 import './App.css';
 
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration : 1000
+    });
+  }, []);
+
   return (
     <div>
+      <div className='orb orb1'></div>
+      <div className='orb orb2'></div>
       <nav>
       <h2>RentX</h2>
       <div className='nav-buttons'>
@@ -28,7 +41,7 @@ function App() {
         />
       </section>
 
-      <section className="stats">
+      <section className="stats" data-aos="zoom-in-up">
         <div className="stat-card">
           <h2>500+</h2>
           <p>Premium Cars</p>
@@ -43,7 +56,7 @@ function App() {
         </div>
       </section>
 
-      <section className='featured'>
+      <section className='featured' data-aos = "fade-up">
 
         <div className='featured-header'>
           <p className='featured-tag'>PREMIUM COLLECTION</p>  
@@ -54,89 +67,118 @@ function App() {
         </div>
 
         <div className='car-grid'>
-          <div className='car-card'>
-            <img src="https://imgd.aeplcdn.com/664x374/n/cw/ec/49376/f8-tributo-exterior-right-rear-three-quarter.jpeg?q=80" alt="Car 1" />
-            <div className='car-overlay'>
-              <div className='car-header'>
-                <div>
-                  <small>FERRARI</small>
-                  <h3>F8 Tributo</h3>
+            <div className="car-card" data-aos="zoom-in-up" data-aos-delay="100" >
+
+              <img
+                src="https://imgd.aeplcdn.com/664x374/n/cw/ec/49376/f8-tributo-exterior-right-rear-three-quarter.jpeg?q=80"
+                alt="Ferrari F8"
+              />
+
+              <div className="car-overlay">
+
+                <div className="car-header">
+                  <div>
+                    <small>FERRARI</small>
+                    <h3>F8 Tributo</h3>
+                  </div>
+
+                  <span>♥</span>
                 </div>
-                <span>♥</span>
+
+                <div className="car-specs">
+                  <span>Automatic</span>
+                  <span>Petrol</span>
+                  <span>710 HP</span>
+                </div>
+
+                <div className="car-footer">
+                  <div className="price">
+                    <span>FROM</span>
+                    <h4>₹5000/day</h4>
+                  </div>
+
+                  <button>Rent Now</button>
+                </div>
+
               </div>
 
-              <div className='car-specs'>
-                <span>Automatic</span>
-                <span>Petrol</span>
-                <span>710 HP</span>
-              </div>
-
-              <div className='car-footer'>
-                <div className='price'>
-                  <span>FROM</span>
-                  <h4>$500/day</h4>
-                </div>
-                <button>Rent Now</button>
-              </div>
             </div>
-          </div>
-          <div className='car-card'>
-            <img src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/masterpieces/huracan_performante_spy/s/huracan_performante_spy_s_03.jpg" alt="Car 2" />
-            <div className='car-overlay'>
-              <div className='car-header'>
-                <div>
-                  <small>LAMBORGINI</small>
-                  <h3>Huracan</h3>
+            <div className="car-card" data-aos="zoom-in-up" data-aos-delay="300">
+
+              <img
+                src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/masterpieces/huracan_performante_spy/s/huracan_performante_spy_s_03.jpg"
+                alt="Lamborgini Huracan"
+              />
+
+              <div className="car-overlay">
+
+                <div className="car-header">
+                  <div>
+                    <small>LAMBORGINI</small>
+                    <h3>Huracan</h3>
+                  </div>
+
+                  <span>♥</span>
                 </div>
-                <span>♥</span>
+
+                <div className="car-specs">
+                  <span>Automatic</span>
+                  <span>Petrol</span>
+                  <span>700 HP</span>
+                </div>
+
+                <div className="car-footer">
+                  <div className="price">
+                    <span>FROM</span>
+                    <h4>₹6000/day</h4>
+                  </div>
+
+                  <button>Rent Now</button>
+                </div>
+
               </div>
 
-              <div className='car-specs'>
-                <span>Automatic</span>
-                <span>Petrol</span>
-                <span>640 HP</span>
-              </div>
-
-              <div className='car-footer'>
-                <div className='price'>
-                  <span>FROM</span>
-                  <h4>$450/day</h4>
-                </div>
-                <button>Rent Now</button>
-              </div>
             </div>
-          </div>
-          <div className='car-card'>
-            <img src="https://hips.hearstapps.com/hmg-prod/images/2025-porsche-911-gt3-touring-005-a9200559-69529b961ef43.jpg?crop=0.571xw:0.482xh;0.168xw,0.360xh&resize=1200:*" alt="Car 3" />
-            <div className='car-overlay'>
-              <div className='car-header'>
-                <div>
-                  <small>PORSCHE</small>
-                  <h3>911 Carrera</h3>
+            <div className="car-card" data-aos="zoom-in-up" data-aos-delay="500">
+
+              <img
+                src="https://hips.hearstapps.com/hmg-prod/images/2025-porsche-911-gt3-touring-005-a9200559-69529b961ef43.jpg?crop=0.571xw:0.482xh;0.168xw,0.360xh&resize=1200:*"
+                alt="Porsche 911"
+              />
+
+              <div className="car-overlay">
+
+                <div className="car-header">
+                  <div>
+                    <small>PORSCHE</small>
+                    <h3>911 Carerra</h3>
+                  </div>
+
+                  <span>♥</span>
                 </div>
-                <span>♥</span>
-              </div>
 
-              <div className='car-specs'>
-                <span>Automatic</span>
-                <span>Petrol</span>
-                <span>701 HP</span>
-
-              </div>
-
-              <div className='car-footer'>
-                <div className='price'>
-                  <span>FROM</span>
-                  <h4>$400/day</h4>
+                <div className="car-specs">
+                  <span>Automatic</span>
+                  <span>Petrol</span>
+                  <span>610 HP</span>
                 </div>
-                <button>Rent Now</button>
+
+                <div className="car-footer">
+                  <div className="price">
+                    <span>FROM</span>
+                    <h4>₹4000/day</h4>
+                  </div>
+
+                  <button>Rent Now</button>
+                </div>
+
               </div>
+
             </div>
-          </div>
         </div>
       </section>
 
-      <section className='why'>
+      <section className='why' data-aos="zoom-in">
         <h2>Why Choose RentX?</h2>
         <div className='why-grid'>
           <div className='why-card'>
@@ -156,7 +198,7 @@ function App() {
         </div>
       </section>
       
-      <section className='testimonials'>
+      <section className='testimonials' data-aos="fade-right">
         <div className='testimonial-header'>
           <p>CUSTOMER REVIEWS</p>
           <h2>What Our Customers Say</h2>
@@ -169,8 +211,13 @@ function App() {
             <p>Renting a Ferrari was effortless. The booking process was smooth and the car arrived in pristine condition</p>
 
             <div className='customer'>
-              <h4>Rahul Sharma</h4>
-              <span>Mumbai</span>
+              <div className='avatar'>
+                R
+              </div>
+              <div className='customer-info'>
+                <h4>Rahul Sharma</h4>
+                <span>Mumbai</span>
+              </div>
             </div>
           </div>
 
@@ -180,23 +227,66 @@ function App() {
             <p>The best luxury car rental experience I've ever had. Transparent pricing and excellent support.</p>
 
             <div className='customer'>
-              <h4>Priya Patel</h4>
-              <span>Bangalore</span>
+              <div className='avatar'>
+                P
+              </div>
+              <div className='customer-info'>
+                <h4>Priya Patel</h4>
+                <span>Bangalore</span>
+              </div>
             </div>
           </div>
 
           <div className='testimonial-card'>
             <div className='stars'>⭐⭐⭐⭐⭐</div>
 
-            <p>Booked a Porsche for a weekend trip. Everything from pickup to return was perfectly managed.</p>
+            <p>The best luxury car rental experience I've ever had. Transparent pricing and excellent support.</p>
             <div className='customer'>
-              <h4>Arjun Singh</h4>
-              <span>Delhi</span>
+              <div className='avatar'>
+                A
+              </div>
+              <div className='customer-info'>
+                <h4>Arjun Singh</h4>
+                <span>Delhi</span>
+              </div>
             </div>
           </div>
 
         </div>
       </section>
+
+      <footer data-aos="fade" data-aos-anchor-placement="top-bottom">
+        <div className='footer-top'>
+          <div className='footer-brand'>
+            <h2>RentX</h2>
+
+            <p>Drive Beyond Ordinary</p>
+          </div>
+
+          <div className='footer-links'>
+            <div>
+              <h4>Quick Links</h4>
+              <p>Home</p>
+              <p>Cars</p>
+              <p>About</p>
+              <p>Contact</p>
+            </div>
+
+            <div>
+              <h4>Support</h4>
+
+              <p>Help Center</p>
+              <p>FAQs</p>
+              <p>Terms</p>
+              <p>Privacy </p>
+            </div>
+          </div>
+        </div>
+
+        <div className='footer-bottom'>
+          <p>© 2026 RentX. All Rights Reserved.</p>
+        </div>
+      </footer>
 
 
     </div>
